4088fb8d92fb5b280b70011cc7ff6b6c0ecc078e
Aslam Hadi Harsono <aslam.hadi@kumparan.com>
2018-05-09 10:05:25 +0700

    add SliceAtoi

--- utils/slice.go
+++ utils/slice.go
@@ -1,5 +1,7 @@
 package utils
 
+import "strconv"
+
 // ContainsInt64 tells whether a slice contains x.
 func ContainsInt64(a []int64, x int64) bool {
 	for _, n := range a {
@@ -8,4 +10,19 @@ func ContainsInt64(a []int64, x int64) bool {
 		}
 	}
 	return false
+}
+
+// SliceAtoi -> convert array of string to array of integer
+func SliceAtoi(s []string) ([]int, bool) {
+	var status = true
+	var arr []int
+
+	for _, val := range s {
+		if i, err := strconv.Atoi(val); err != nil {
+			status = false
+		} else {
+			arr = append(arr, i)
+		}
+	}
+	return arr, status
 }
